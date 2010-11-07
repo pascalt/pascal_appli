145c8d6090def0946abd1dae6dff7788754d0edd
Pascal Toutain <pascaltoutain@pascal-2.local>
2010-11-07 19:21:51 +0100

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PascalAppli::Application.config.secret_token = 'f6171ed76464f3c41911daa844c169e7c466581f59111ad0931846afed6a344df51c14fcaf0feaae3df5e406fbb064841c1a276e7ef2b6af12653d33a5f3af25'
