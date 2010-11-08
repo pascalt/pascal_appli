d61d12cc398889243d50da6f9af65cac31648235
Pascal Toutain <pascaltoutain@gmail.com>
2010-11-08 09:38:51 +0100

    mes premiers tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'menu'" do
+    it "devrait avoir un H1 Menu" do
+      get 'menu'
+      response.should have_selector("h1", :content=>"Menu")
+    end
     it "should be successful" do
       get 'menu'
       response.should be_success
     end
+   
+    
   end
 
 end
